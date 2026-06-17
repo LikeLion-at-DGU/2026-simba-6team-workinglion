c49d74ea04281be100e012f0ad66da7050c0bc62
dPwls52 <limyj05201@gmail.com>
2026-06-18 02:36:08 +0900

    팟만들기

--- main/urls.py
+++ main/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('dashboard/', dashboard, name='dashboard'),
     path('create_pot/', create_pot, name='create_pot'),
     path('join_pot/', join_pot, name='join_pot'),
+    path('new_pot/', new_pot, name='new_pot'), 
+    path('create_pot/', create_pot, name='create_pot'),
 ]
